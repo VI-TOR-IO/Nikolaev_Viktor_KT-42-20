03d96dbb2c2ff90efe9946d010bbd4395b680079
VI-TOR-IO <vitya.nikolaev9@gmail.com>
2023-10-12 15:24:07 +0300

    add: UnitTests

--- Models/Group.cs
+++ Models/Group.cs
@@ -1,8 +1,14 @@
-﻿namespace _3_lab.Models
+﻿using System.Text.RegularExpressions;
+
+namespace _3_lab.Models
 {
     public class Group
     {
         public int GroupId { get; set; }
         public string GroupName { get; set; }
-    }
+        public bool IsValidGroupName()
+        {
+            return Regex.Match(GroupName, @"/\D*-\d*-\d\d/g").Success;
+        }
+    }  
 }
